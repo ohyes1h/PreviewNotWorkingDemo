6cf0ec45516c2db4517963cf0de2917554510f25
Sih <ohyes1h@icloud.com>
2025-06-01 16:14:48 +0800

    Initial Commit

---
+++ PreviewNotWorkingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PreviewNotWorkingDemo
+//
+//  Created by Sih Ou-Yang on 2025/6/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PreviewNotWorkingDemo/PreviewNotWorkingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewNotWorkingDemoApp.swift
+//  PreviewNotWorkingDemo
+//
+//  Created by Sih Ou-Yang on 2025/6/1.
+//
+
+import SwiftUI
+
+@main
+struct PreviewNotWorkingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
