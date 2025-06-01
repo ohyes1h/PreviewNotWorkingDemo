c045097c0592cfba3c07107f4fb2517a5ab89a6a
Sih <ohyes1h@icloud.com>
2025-06-01 16:27:43 +0800

    feat: add dependency

--- PreviewNotWorkingDemo/PreviewNotWorkingDemoApp.swift
+++ PreviewNotWorkingDemo/PreviewNotWorkingDemoApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct PreviewNotWorkingDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Custom initialization code if needed
+        FirebaseApp.configure()
+        return true
     }
 }
